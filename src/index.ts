532ad8aa13f92a1e61ad8452f82bfb26d314ef4d
Osama Hassanein <player1os@gmail.com>
2017-10-16 17:53:54 +0200

    Implement the library's functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello') // tslint:disable-line no-console
+// Load npm modules.
+import * as fse from 'fs-extra'
+
+// Load node modules.
+import * as path from 'path'
+
+export const aquire = async (directoryPath: string, name = '') => {
+	// Use a lockfile to determine whether a backup operation is already running.
+	try {
+		await fse.writeFile(path.join(directoryPath, `${name}.lock`), 'wx')
+	} catch (err) {
+		return false
+	}
+	return true
+}
+
+export const release = async (directoryPath: string, name = '') => {
+	// Remove the lockfile.
+	await fse.unlink(path.join(directoryPath, `${name}.lock`))
+}
