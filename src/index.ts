b05edb5d1a7fb551848f70047854199a930f0c58
Osama Hassanein <player1os@gmail.com>
2017-10-28 23:00:57 +0200

    Upgrade and fix issues

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import * as path from 'path'
 export const aquire = async (directoryPath: string, name = '') => {
 	// Use a lockfile to determine whether a backup operation is already running.
 	try {
-		await fse.writeFile(path.join(directoryPath, `${name}.lock`), 'wx')
+		await fse.writeFile(path.join(directoryPath, `${name}.lock`), '', { flag: 'wx' })
 	} catch (err) {
 		return false
 	}
